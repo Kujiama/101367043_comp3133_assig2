6c91e18f79448e800d7d7b0393a45499b1e53f9a
Kujiama <englandpelenio17@gmail.com>
2024-04-06 10:31:44 -0400

    change uri to cyclic backend for deployment

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -3,8 +3,8 @@ import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'http://localhost:4000/graphql'; // <-- add the URL of the GraphQL server here locally
-// const uri = 'https://tame-calf-buckle.cyclic.app/graphql'; // <-- add the URL of the GraphQL server here in production
+// const uri = 'http://localhost:4000/graphql'; // <-- add the URL of the GraphQL server here locally
+const uri = 'https://curious-boot-bull.cyclic.app/graphql'; // <-- add the URL of the GraphQL server here in production
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
